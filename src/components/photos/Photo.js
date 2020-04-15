1f2ff1ad6a451854ef51fb20f4385bef4144d592
joegturner <59180847+joegturner@users.noreply.github.com>
2020-04-14 20:32:02 -0400

    Create Photo.js

---
+++ src/components/photos/Photo.js
@@ -0,0 +1,13 @@
+// stateless
+import React from 'react';
+
+const Photo = ({ id, farm, server, secret }) => {
+
+    return (
+        <li>
+            <img src={`https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`} alt="" />
+        </li>
+    )
+}
+
+export default Photo;
