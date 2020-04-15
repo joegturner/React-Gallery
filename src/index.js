e2849358bab2f5078c536ddb7f41fc31b9d4831a
joegturner <59180847+joegturner@users.noreply.github.com>
2020-04-14 20:34:01 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import './css/index.css';
 import App from './components/App';
 
 ReactDOM.render(
-  // <React.StrictMode>
     <App />,
-  // {/* </React.StrictMode>, */}
   document.getElementById('root')
 );
 
