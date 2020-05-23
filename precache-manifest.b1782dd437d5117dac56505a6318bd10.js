0f837eba163795dc84edab5766fa3592a0ccd547
joegturner <59180847+joegturner@users.noreply.github.com>
2020-05-23 03:29:02 -0400

    Updates

---
+++ precache-manifest.b1782dd437d5117dac56505a6318bd10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4ee1bd7558cd461939be2d2c4ae33f",
+    "url": "/React-Gallery/index.html"
+  },
+  {
+    "revision": "5ec4eb053a21d9ca7328",
+    "url": "/React-Gallery/static/css/main.8325ea2a.chunk.css"
+  },
+  {
+    "revision": "bd555aa41076911bafdc",
+    "url": "/React-Gallery/static/js/2.fc16954c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Gallery/static/js/2.fc16954c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ec4eb053a21d9ca7328",
+    "url": "/React-Gallery/static/js/main.377bab07.chunk.js"
+  },
+  {
+    "revision": "7e591912b04e55edcdb3",
+    "url": "/React-Gallery/static/js/runtime-main.b23b05a3.js"
+  }
+]);
